b9e436489085678f79760958dbc3a2df97656f46
Diogo Nunes <38764693+ICWeiner@users.noreply.github.com>
2022-04-30 06:29:33 -0700

    finished phrases.c

--- TP/q1/phrases.c
+++ TP/q1/phrases.c
@@ -2,51 +2,52 @@
 #include <string.h>
 
 
-
 int main(int argc, char *argv[]){
 
 	FILE *  fp;
+	int writeText;
+	int lines = 1;
+	char currChar;
 
 	if( argc == 2){
 		fp= fopen(argv[1], "r");
-	}else if( argc == 3){
-		if(argv[1] == "-l"){
-			fp = fopen(argv[2], "r");
-		}else{
-			printf("unkown argument");
-			return 1;
-		}
-		
+		writeText = -1;
+	}else if( argc == 3 && strcmp(argv[1], "-l") == 0){
+		fp = fopen(argv[2], "r");
+		writeText = 0;
 	}else{
 		printf("usage: phrases [-l] file\n");
 		return 1;
 	}
 
 	if (!fp){
-	    printf("couldn read file\n");
-	    return 1;
+		printf("couldn read file\n");
+		return 1;
+	}
+	
+
+	if(writeText == 0){
+		printf("[%d] ",lines);
+
+		while( (currChar = fgetc(fp)) != EOF){
+			if(currChar!='\n') printf("%c",currChar);
+			
+			if(currChar == '.' || currChar == '!' || currChar == '?'){
+				lines++;
+				printf("\n[%d]",lines);
+			}
+		}
+	}else{
+		while( (currChar = fgetc(fp)) != EOF){
+			if(currChar == '.' || currChar == '!' || currChar == '?'){
+				lines++;
+			}
+		}
+		printf("%d",lines);
 	}
 
-	int lines = 0;
-
 	
-	char currChar;
-	//currChar = fgetc(fp);
-	printf("[%d] ",lines + 1);
-
-	while(currChar != EOF){
-		if (currChar = fgetc(fp) ) == EOF;
-        printf("%c",currChar);
-        
-
-        if(currChar == '.' || currChar == '!' || currChar == '?'){
-        	if( (currChar = fgetc(fp) ) == EOF){
-        		break;
-        	}else{
-				lines++;
-				printf("\n[%d] ",lines + 1);
-        	}
-        }
-    }
 	fclose(fp);
+	printf("\n");
+	return 0;
 }
