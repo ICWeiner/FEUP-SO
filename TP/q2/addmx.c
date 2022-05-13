14c145ed3c911acffc27efee377159bc69e63eb4
CarlosMealha <up202005954@edu.fc.up.pt>
2022-05-13 15:55:56 +0100

    some changes but nothing done

--- TP/q2/addmx.c
+++ TP/q2/addmx.c
@@ -4,45 +4,63 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main(int argc, char* argv[]){
-  // check if exactly two arguments are present 
-  if(argc != 3){
-    fprintf(stderr,"usage: addmx file1 file2\n");
+int main(int argc, char *argv[])
+{
+  FILE *f1, f2;
+
+  // check if exactly two arguments are present
+  if (argc != 3)
+  {
+    fprintf(stderr, "usage: addmx file1 file2\n");
     exit(EXIT_FAILURE);
   }
 
   // check if file one can be opened and is readable
-  int f_one = open(argv[1], O_RDONLY);
-  if (f_one == -1) {
-    fprintf(stderr,"error: cannot open %s\n", argv[1]);
+  f1 = fopen(argv[1], "r");
+  if (f1 == -1)
+  {
+    fprintf(stderr, "error: cannot open %s\n", argv[1]);
     exit(EXIT_FAILURE);
   }
 
   // check if file two can be opened and is readable
   int f_two = open(argv[2], O_RDONLY);
-  if (f_two == -1) {
-    fprintf(stderr,"error: cannot open %s\n", argv[2]);
+  if (f_two == -1)
+  {
+    fprintf(stderr, "error: cannot open %s\n", argv[2]);
     exit(EXIT_FAILURE);
   }
 
-  char* l_one;
-  char* l_two;
+  char *l_one;
+  char *l_two;
 
   // check if line from file one was read to char pointer
-  if(fgets (l_one, 4, argv[1])==NULL) {
-    fprintf(stderr,"error: cannot read %s\n",argv[1]);
+  /*if (fgets(l_one, 4, argv[1]) == NULL)
+  {
+    fprintf(stderr, "error: cannot read %s\n", argv[1]);
     exit(EXIT_FAILURE);
   }
 
   // check if line from file two was read to char pointer
-  if(fgets (l_two, 4, argv[2])==NULL) {
-    fprintf(stderr,"error: cannot read %s\n",argv[2]);
+  if ((l_two, 4, argv[2]) == NULL)
+  {
+    fprintf(stderr, "error: cannot read %s\n", argv[2]);
     exit(EXIT_FAILURE);
   }
 
   int result = strcmp(l_one, l_two);
-  if(result != 0){
-    fprintf(stderr,"error: matrices have different sizes\n");
+  if (result != 0)
+  {
+    fprintf(stderr, "error: matrices have different sizes\n");
     exit(EXIT_FAILURE);
-  }
+  } */
+
+  // ler o ficheiro e por dentro do array
+  // e depois meter esse array para dentro da memoria partilhada
+
+  int fscanf(argv[1], )
+
+      while ()
+
+          int *shared_memory = mmap(NULL, 10, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
 }
