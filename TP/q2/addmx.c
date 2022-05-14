48730460015f7aa6c09d6e9685723d9f76ff8c02
Diogo <up202006059@up.pt>
2022-05-15 00:32:01 +0100

    its cool reading matrices

--- TP/q2/addmx.c
+++ TP/q2/addmx.c
@@ -6,7 +6,8 @@
 
 int main(int argc, char *argv[])
 {
-  FILE *f1, f2;
+  FILE *f1;
+  FILE *f2;
 
   // check if exactly two arguments are present
   if (argc != 3)
@@ -17,22 +18,41 @@ int main(int argc, char *argv[])
 
   // check if file one can be opened and is readable
   f1 = fopen(argv[1], "r");
-  if (f1 == -1)
+  if (f1 == NULL)
   {
     fprintf(stderr, "error: cannot open %s\n", argv[1]);
     exit(EXIT_FAILURE);
   }
 
   // check if file two can be opened and is readable
-  int f_two = open(argv[2], O_RDONLY);
-  if (f_two == -1)
+  f2 = fopen(argv[2], "r");
+  if (f2 == NULL)
   {
     fprintf(stderr, "error: cannot open %s\n", argv[2]);
     exit(EXIT_FAILURE);
   }
 
-  char *l_one;
-  char *l_two;
+  // gets two first digits too check if matrices are equal
+  int m1_x, m1_y, m2_x, m2_y;
+  char buffer1, buffer2;
+
+  fscanf(f1, "%d %c %d",&m1_x,&buffer1,&m1_y);
+  fscanf(f2, "%d %c %d",&m2_x,&buffer2,&m2_y);
+
+  if (m1_x != m2_x || m1_y != m2_y)
+  {
+    fprintf(stderr, "error: matrices have different sizes\n");
+    exit(EXIT_FAILURE);
+  } 
+
+  int array1[m1_x*m1_y];
+  int array2[m2_x*m2_y];
+
+  for(int i=0; i<m1_x*m1_y ;i++)
+    fscanf(f1, "%d",&array1[i]);
+  
+  for(int i=0; i<m2_x*m2_y ;i++)
+    fscanf(f2, "%d",&array2[i]);
 
   // check if line from file one was read to char pointer
   /*if (fgets(l_one, 4, argv[1]) == NULL)
@@ -40,20 +60,18 @@ int main(int argc, char *argv[])
     fprintf(stderr, "error: cannot read %s\n", argv[1]);
     exit(EXIT_FAILURE);
   }
-
   // check if line from file two was read to char pointer
   if ((l_two, 4, argv[2]) == NULL)
   {
     fprintf(stderr, "error: cannot read %s\n", argv[2]);
     exit(EXIT_FAILURE);
   }
-
   int result = strcmp(l_one, l_two);
   if (result != 0)
   {
     fprintf(stderr, "error: matrices have different sizes\n");
     exit(EXIT_FAILURE);
-  } */
+  } 
 
   // ler o ficheiro e por dentro do array
   // e depois meter esse array para dentro da memoria partilhada
@@ -63,4 +81,7 @@ int main(int argc, char *argv[])
       while ()
 
           int *shared_memory = mmap(NULL, 10, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, -1, 0);
+}
+
+*/
 }
